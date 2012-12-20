7be7680cc6ae1e278aa30a28a19d78dce7531fc6
mathieuzen <zen.mathieu@gmail.com>
2012-12-20 18:52:08 +0100

    Metrics of Density and Balance

--- src/aesthetixy/appspot/com/client/Density.java
+++ src/aesthetixy/appspot/com/client/Density.java
@@ -7,40 +7,34 @@ import java.util.ArrayList;
 import com.google.gwt.i18n.client.NumberFormat;
 
 
-public class Density {
+public class Density extends Metric {
 
-	static String texte ="";
 	static float density;
 	static float area = 0;
 	static float areaframe;
 	NumberFormat fmt = NumberFormat.getFormat("##0.##");
 
-	public Density(int[][] rectXY, int frameWidth, int frameHeight) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static float getDensity(ArrayList rectXY, int frameWidth, int frameHeight) {
-		density=0;
+	public static double calculate(ArrayList rectXY, int frameWidth, int frameHeight) {
+		value = 0;
 		areaframe=0;
 		area=0;
-			for(int i=1; i<rectXY.size(); i++){
-				Region r = (Region) rectXY.get(i);
-				if(r.getX()!=0 || r.getY()!=0)
-				{
-					area += r.getWidth()*r.getHeight();
-				}
-				
-				areaframe = frameWidth*frameHeight;
-			
-				density = area/areaframe;
-			
+		
+		for(int i=0; i<rectXY.size(); i++){
+			Region r = (Region) rectXY.get(i);
+			if(r.getX()!=0 || r.getY()!=0)
+			{
+				area += r.getWidth()*r.getHeight();
 			}
 			
-			//Window.alert("Frame area: "+areaframe+"\nObjects area: "+area+"\nDensity: "+fmt.format(density));
-			return density;
-	
+			areaframe = frameWidth*frameHeight;
+		
+			value = area/areaframe;
+		
+		}
+		
+		return value;
+
 	}
-
-
-
+	
 }
+	
--- src/aesthetixy/appspot/com/client/MetricsReport.java
+++ src/aesthetixy/appspot/com/client/MetricsReport.java
@@ -7,37 +7,44 @@ import com.google.gwt.user.client.Window;
 
 public class MetricsReport {
 	NumberFormat fmt = NumberFormat.getFormat("##0.##");
-	float density = 0;
-	float balance = 0;
+	double density = 0;
+	double balance = 0;
 
 	public MetricsReport(ArrayList rectXY, int frameWidth, int frameHeight){
+			if(rectXY.isEmpty()){
+				Window.alert("You must draw at least 1 element in order to compute metrics.");
+			}
 			
-			density = Density.getDensity(rectXY, frameWidth, frameHeight);
-			String msgDensity;
-			if(density<0.2||density>0.8){
-				msgDensity = "Bad !";
-			}
-			else if(density<0.4||density>0.6){
-				msgDensity = "Ok...";
-			}
 			else{
-				msgDensity = "Good !";
-			}
-			/*	
-			balance = Balance.getBalance(rectXY, frameWidth, frameHeight);
-			String msgBalance;
-			if(balance<0.4){
-				msgBalance = "Bad !";
-			}
-			else if(balance<0.7){
-				msgBalance = "Ok...";
-			}
-			else{
-				msgBalance = "Good !";
-			}*/
+				density = Density.calculate(rectXY, frameWidth, frameHeight);
+				String msgDensity;
+				if(density<0.2||density>0.8){
+					msgDensity = "Bad !";
+				}
+				else if(density<0.4||density>0.6){
+					msgDensity = "Ok...";
+				}
+				else{
+					msgDensity = "Good !";
+				}
+				
+				balance = Balance.calculate(rectXY, frameWidth, frameHeight);
+				String msgBalance;
+				if(balance<0.5){
+					msgBalance = "Bad !";
+				}
+				else if(balance<0.8){
+					msgBalance = "Ok...";
+				}
+				else{
+					msgBalance = "Good !";
+				}
+				
 			
-			Window.alert("METRIC REPORT\n Density = "+fmt.format(density)+"  "+msgDensity);
-			//Window.alert("METRIC REPORT\n Density = "+fmt.format(density)+"  "+msgDensity+"\n Balance = "+fmt.format(balance)+"  "+msgBalance);
-			
+			Window.alert("METRIC REPORT\n Density = "+fmt.format(density)+"  "+msgDensity
+						+"\n Balance = "+fmt.format(balance)+"  "+msgBalance
+					);
+			}
+		
 	}
 }
