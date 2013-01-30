acf447d6ac179facab22aaac985767db5d044b8f
mathieuzen <zen.mathieu@gmail.com>
2013-01-30 13:43:53 +0100

    Metrics computed on server-side

--- src/aesthetixy/appspot/com/client/Main.java
+++ src/aesthetixy/appspot/com/client/Main.java
@@ -2,6 +2,8 @@ package aesthetixy.appspot.com.client;
 
 import java.util.ArrayList;
 
+import aesthetixy.appspot.com.server.MetricsReport;
+
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.core.client.EntryPoint;
